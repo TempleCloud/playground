1e39b39f33356b77a1bdf5933902a0399f33c499
TempleCloud <tim.langford@gmail.com>
2017-05-01 21:27:11 +0100

    Added password encryption and basic unit test.

--- spring-microservices-in-action/03-memoservice/memo/memoservice/src/main/java/com/xepsa/memo/Application.java
+++ spring-microservices-in-action/03-memoservice/memo/memoservice/src/main/java/com/xepsa/memo/Application.java
@@ -2,6 +2,7 @@ package com.xepsa.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ public class Application {
     }
 
     @RequestMapping(value="/{toEcho}",method = RequestMethod.GET)
+    @RefreshScope   // enable config refresh enpoint: http://<yourserver>:<port>/refresh
     public String hello( @PathVariable("toEcho") String toEcho) {
         return String.format("{\"message\":\"%s\"}", toEcho);
     }
--- spring-microservices-in-action/03-memoservice/memo/memoservice/src/main/java/com/xepsa/memo/services/MemoService.java
+++ spring-microservices-in-action/03-memoservice/memo/memoservice/src/main/java/com/xepsa/memo/services/MemoService.java
@@ -1,30 +1,41 @@
 package com.xepsa.memo.services;
 
+import com.xepsa.memo.config.ServiceConfig;
 import com.xepsa.memo.model.Memo;
+import com.xepsa.memo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RefreshScope
 public class MemoService {
 
-    public Memo getMemo(String MemoId){
-        return new Memo()
-                .withId(MemoId)
-                .withUserId(UUID.randomUUID().toString())
-                .withMessage("Some Message");
+    @Autowired
+    private MemoRepository memoRepository;
+
+    @Autowired
+    ServiceConfig config;
+
+
+    public Memo getMemo(String memoId){
+        Memo memo = memoRepository.findById(memoId);
+        return memo.withExtraInfo(config.getExampleProperty());
     }
 
-    public void saveMemo(Memo Memo) {
-
+    public void saveMemo(Memo memo) {
+        memo.withId(UUID.randomUUID().toString());
+        memoRepository.save(memo);
     }
 
-    public void updateMemo(Memo Memo) {
-
+    public void updateMemo(Memo memo) {
+        memoRepository.save(memo);
     }
 
-    public void deleteMemo(Memo Memo) {
-
+    public void deleteMemo(Memo memo) {
+        memoRepository.delete(memo.getId());
     }
 
 }
