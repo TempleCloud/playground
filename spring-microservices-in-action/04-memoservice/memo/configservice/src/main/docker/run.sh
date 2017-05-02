84f182bf8fc0523a74b155acde955b97f60dc10d
TempleCloud <tim.langford@gmail.com>
2017-05-02 16:08:41 +0100

    Added discovery service and memoservice registration.

--- spring-microservices-in-action/04-memoservice/memo/configservice/src/main/docker/run.sh
+++ spring-microservices-in-action/04-memoservice/memo/configservice/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting Config Service"
 echo "********************************************************"
-java -jar /usr/local/configserver/@project.build.finalName@.jar
+java -jar /usr/local/config-service/@project.build.finalName@.jar
--- spring-microservices-in-action/04-memoservice/memo/memoservice/src/main/java/com/xepsa/memo/Application.java
+++ spring-microservices-in-action/04-memoservice/memo/memoservice/src/main/java/com/xepsa/memo/Application.java
@@ -3,12 +3,14 @@ package com.xepsa.memo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 @RequestMapping(value="v1/echo")
 public class Application {
