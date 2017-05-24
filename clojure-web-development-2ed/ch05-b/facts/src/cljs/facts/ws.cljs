394cba79f218944a7db34ee1f271b25808217da8
TempleCloud <tim.langford@gmail.com>
2017-05-24 23:13:07 +0100

    Removed commented code.

--- clojure-web-development-2ed/ch05-b/facts/src/cljs/facts/ws.cljs
+++ clojure-web-development-2ed/ch05-b/facts/src/cljs/facts/ws.cljs
@@ -1,38 +1,6 @@
 (ns facts.ws
   (:require [taoensso.sente :as sente]))
 
-;; (defonce ws-chan (atom nil))
-
-;; (def json-reader
-;;   (t/reader :json))
-
-;; (def json-writer
-;;   (t/writer :json))
-
-;; (defn receive-fact!
-;;   [handler]
-;;   (fn [fact]
-;;     (->> fact
-;;          .-data
-;;          (t/read json-reader)
-;;          handler)))
-
-;; (defn send-fact!
-;;   [fact]
-;;   (if @ws-chan
-;;     (->> fact
-;;          (t/write json-writer)
-;;          (.send @ws-chan))
-;;     (throw (js/Error. "WebSocket is not available!"))))
-
-;; (defn connect!
-;;   [url receive-handler]
-;;   (if-let [chan (js/WebSocket. url)]
-;;     (do
-;;       (set! (.-onmessage chan) (receive-fact! receive-handler))
-;;       (reset! ws-chan chan))
-;;     (throw (js/Error. "WebSocket connection failed!"))))
-
 (let
   [connection (sente/make-channel-socket! "/ws" {:type :auto})]
   (def ch-chsk (:ch-recv connection))
