1add5355a529f61bdb40939aeea716f14c1ed364
TempleCloud <tim.langford@gmail.com>
2017-06-19 01:28:27 +0100

    A small k8s tutorial.

---
+++ kubernetes/k8s-clj-mongo/init_env.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------------------------------------
+# docker
+
+export mongo_name='mongo'
+function run_mongo() {
+    docker run -d \
+        --name ${mongo_name} \
+        -p 27017:27017 \
+        ${mongo_name}
+}
+
+export app_name='notes'
+export database_url='mongodb://192.168.0.6/noddy_facts_dev'
+function run_notes() {
+    docker run -d \
+        --name ${app_name} \
+        -p 3000:3000 \
+        templecloud/noddy-reagent-mongo \
+        -Ddatabase-url=${database_url} -jar /app.jar
+}
+
+#---------------------------------------------------------------------------------------------------
+# glcoud
+
+export gpid='trjl-158912'
+export k8s_name='k8s-cluster'
+export k8s_zone='europe-west1-b'
+export k8s_machine_type='f1-micro'
+
+function gcloud_login() {
+    gcloud auth login
+    gcloud components update
+	gcloud config set project ${gpid}
+}
+
+function gcloud_cluster_create() {
+	gcloud container \
+  		--project ${gpid} \
+  	clusters create ${k8s_name} \
+  		--zone ${k8s_zone} \
+  		--machine-type ${k8s_machine_type} \
+  		--num-nodes "3" \
+  		--network "default"
+}
+
+function gcloud_cluster_destroy() {
+	gcloud container \
+  		--project ${gpid} \
+  	clusters delete ${k8s_name} \
+  		--zone ${k8s_zone}
+}
+
+function gcloud_cluster_login() {
+	gcloud container clusters get-credentials ${k8s_name}
+}
+
+function gcloud_forwarding_rules() {
+	gcloud compute forwarding-rules list
+}
+
+export k8s_disk01_name='mongo-disk'
+export k8s_disk01_size=200GB
+
+function gcloud_disk_create() {
+	gcloud compute disks create \
+		--project ${gpid} \
+		--zone ${k8s_zone} \
+		--size 200GB \
+		${k8s_disk01_name}
+}
+
+function gcloud_disk_destroy() {
+	gcloud compute disks delete \
+		--zone ${k8s_zone} \
+		${k8s_disk01_name}
+}
+
+
+#---------------------------------------------------------------------------------------------------
+# kubectl
+
+function k8s_deploy() {
+	kubectl create -f mongodb-controller.yml
+	kubectl create -f mongodb-service.yml
+	kubectl create -f notes-app-controller.yml
+	kubectl create -f notes-app-service.yml
+}
+
+function k8s_clean() {
+	kubectl delete pods,services --all
+}
+
